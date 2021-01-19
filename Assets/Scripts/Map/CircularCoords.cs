ba0afbce4af2dd9403b934772f1f70302b14fadb
Mika Drakolis <mika.drakolis@gmail.com>
2021-01-19 19:48:13 +0100

    Circular generation

--- Assets/Scripts/Map/CircularCoords.cs
+++ Assets/Scripts/Map/CircularCoords.cs
@@ -1,17 +1,29 @@
+using System;
+using UnityEngine;
+
+
 public class CircularCoords {
-  private float _radius;
-  private float _angle;
+    private float _angle;
 
-  public float radius { get => _radius; set => _radius = value; }
-  public float angle {
-    get => _angle;
-    set => {
-      _angle = Math.Max(Math.Min(0, value), 2*Math.PI);
+    public float radius {
+        get; set;
     }
-  }
 
-  public CircularCoords(float radius, float angle) {
-    this.radius = radius;
-    this.angle = angle;
-  }
+    public float angle {
+        get => _angle;
+        set {
+            _angle = Mathf.Min(Mathf.Max(0, value), 2 * Mathf.PI);
+        }
+    }
+
+    public CircularCoords(float radius, float angle) {
+        this.radius = radius;
+        this.angle = angle;
+    }
+
+    public Vector2 ToVectorFrom(float centerX, float centerY) {
+        float y = radius * Mathf.Sin(angle) + centerY;
+        float x = radius * Mathf.Cos(angle) + centerX;
+        return new Vector2(x, y);
+    }
 }
--- Assets/Scripts/Map/StarMap.cs
+++ Assets/Scripts/Map/StarMap.cs
@@ -3,25 +3,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StarMap : MonoBehaviour
-{
+public class StarMap : MonoBehaviour {
     public int mapLevels;
+    public float delta = 0.1F;
+    public GameObject mapRoot {
+        get => transform.gameObject;
+    }
+    public GameObject testEmptySystem;
 
-    private List<Tuple<float, float>> generationCoords;
+    private List<CircularCoords> systemsCircularCoords = new List<CircularCoords>();
+    private List<Vector2> systemsVectorCoords = new List<Vector2>();
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+        for (int i = 0; i < mapLevels; i++) {
+            systemsCircularCoords.AddRange(GenerateOneSystemLevel(i));
+        }
+        foreach (var systemCircular in systemsCircularCoords) {
+            Vector2 systemVector = systemCircular.ToVectorFrom(mapRoot.transform.position.x, mapRoot.transform.position.y);
+            systemsVectorCoords.Add(systemVector);
+            Debug.Log(systemVector);
+            Instantiate(testEmptySystem, new Vector3(systemVector.x, systemVector.y, 0), new Quaternion(), mapRoot.transform);
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
     }
 
-    Tuple<float, float> GenerateOneSystem(int level) {
-      if(level == 0) {
+    List<CircularCoords> GenerateOneSystemLevel(int level) {
+        List<CircularCoords> systems = new List<CircularCoords>();
 
-      }
+        if (level == 0) {
+            systems.Add(new CircularCoords(0, 0));
+            return systems;
+        }
+        float angles = 2 * (4 * level - 2);
+        float angleStep = 2 * Mathf.PI / angles;
+
+        for (int i = 1; i <= angles; i++) {
+            float minRadius = level - 1 + delta;
+            float maxRadius = level;
+            float minAngle = (i - 1) * angleStep + delta;
+            float maxAngle = i * angleStep - delta;
+
+            float myRadius = UnityEngine.Random.Range(minRadius, maxRadius);
+            float myAngle = UnityEngine.Random.Range(minAngle, maxAngle);
+
+            Debug.Log($"Generating star {i}/{angles} for level {level}");
+            Debug.Log($"Minimal radius {minRadius}");
+            Debug.Log($"Maximal radius {maxRadius}");
+            Debug.Log($"Minimal angle {minAngle}");
+            Debug.Log($"Maximal angle {maxAngle}");
+
+            Debug.Log($"My radius {myRadius}");
+            Debug.Log($"My angle {myAngle}");
+
+
+            systems.Add(new CircularCoords(myRadius, myAngle));
+        }
+        return systems;
     }
 }
